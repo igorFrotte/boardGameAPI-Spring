ea359f411b78fb93f4186e722e3aa87922632d4d
igorFrotte <igorfrotte@gmail.com>
2024-02-08 17:53:05 -0300

    GameModel ready

---
+++ src/main/java/com/board/api/models/GameModel.java
@@ -0,0 +1,44 @@
+package com.board.api.models;
+
+import com.board.api.dtos.GameDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "games")
+public class GameModel {
+    
+    public GameModel(GameDTO dto){  
+        this.name = dto.getName();
+        this.image = dto.getImage();
+        this.stockTotal = dto.getStockTotal();
+        this.pricePerDay = dto.getPricePerDay();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 150, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private int stockTotal;
+
+    @Column(nullable = false)
+    private int pricePerDay;
+}
