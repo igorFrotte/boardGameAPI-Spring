84ab755271b473b33135e66a9272463e08854d50
igorFrotte <igorfrotte@gmail.com>
2024-02-08 18:12:23 -0300

    GameService ready

---
+++ src/main/java/com/board/api/services/GameService.java
@@ -0,0 +1,31 @@
+package com.board.api.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.board.api.dtos.GameDTO;
+import com.board.api.models.GameModel;
+import com.board.api.repositories.GameRepository;
+
+@Service
+public class GameService {
+    final GameRepository gameRepository;
+
+    GameService(GameRepository gameRepository){
+        this.gameRepository = gameRepository;
+    }
+
+    public List<GameModel> findAll(){
+        return gameRepository.findAll();
+    }
+
+    public GameModel save (GameDTO dto){
+        if(gameRepository.existsByName(dto.getName())){
+            // exception
+        }
+
+        GameModel game = new GameModel(dto);
+        return gameRepository.save(game);
+    }
+}
