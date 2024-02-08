f19e25d10c6c4fdaafe1a2fd370a5d725a73b50b
igorFrotte <igorfrotte@gmail.com>
2024-02-08 19:13:36 -0300

    ClientService ready

---
+++ src/main/java/com/board/api/services/ClientService.java
@@ -0,0 +1,33 @@
+package com.board.api.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.board.api.dtos.ClientDTO;
+import com.board.api.models.ClientModel;
+import com.board.api.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+    final ClientRepository clientRepository;
+
+    ClientService(ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
+    }
+
+    @SuppressWarnings("null")
+    public Optional<ClientModel> findById(Long id){
+        return clientRepository.findById(id); //exception
+    }
+
+    public ClientModel save (ClientDTO dto){
+        if(clientRepository.existsByCpf(dto.getCpf())){
+            //exception
+        }
+
+        ClientModel customer = new ClientModel(dto);
+        return clientRepository.save(customer);
+    }
+
+}
